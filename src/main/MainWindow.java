8fe5603c653bd3656c485c17987c5353bf0ab7b3
aeternum-dev <viccesvok4@gmail.com>
2023-02-19 22:25:07 +0100

    Add methods for arithmetic operations

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -6,16 +6,21 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainWindow extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("MainWindowInterface.fxml")));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindowInterface.fxml"));
+        Scene scene = new Scene(loader.load());
         stage.setScene(scene);
         scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setResizable(false);
         stage.setTitle("Calculator");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
+
+        ((MainWindowController)loader.getController()).init(stage);
         stage.show();
     }
 
--- src/main/MainWindowController.java
+++ src/main/MainWindowController.java
@@ -1,13 +1,76 @@
 package main;
 
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class MainWindowController implements Initializable {
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+public class MainWindowController {
+    @FXML private Pane titlePane;
+    @FXML private ImageView btnClose;
+    @FXML private ImageView btnMinimize;
+    @FXML private Label calculatorOutput;
+
+    private double x, y;
+    private double num1 = 0;
+    private String operator = "+";
+
+    public void init(Stage stage) {
+        titlePane.setOnMousePressed(mouseEvent -> {
+            x= mouseEvent.getSceneX();
+            y= mouseEvent.getSceneY();
+        });
+        titlePane.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+
+        btnClose.setOnMouseClicked(mouseEvent -> stage.close());
+        btnMinimize.setOnMouseClicked((mouseEvent -> stage.setIconified(true)));
+    }
+
+    @FXML
+    void onNumberClicked(MouseEvent event) {
+        int value = Integer.parseInt(((Pane)event.getSource()).getId().replace("btn",""));
+        calculatorOutput.setText(Double.parseDouble(calculatorOutput.getText())==0?String.valueOf((double)value)
+                :String.valueOf(Double.parseDouble(calculatorOutput.getText())*10+value));
+    }
+
+    @FXML
+    void onSymbolClicked(MouseEvent event) {
+        String symbol = ((Pane)event.getSource()).getId().replace("btn","");
+
+        if(symbol.equals("Equation")) {
+            double num2 = Double.parseDouble(calculatorOutput.getText());
+            switch (operator) {
+                case  "+" -> calculatorOutput.setText((num1+num2) + "");
+                case  "-" -> calculatorOutput.setText((num1-num2) + "");
+                case  "*" -> calculatorOutput.setText((num1*num2) + "");
+                case  "/" -> calculatorOutput.setText((num1/num2) + "");
+            }
+            operator = ".";
+        }
+        else if(symbol.equals("Clear")) {
+            calculatorOutput.setText(String.valueOf(0.0));
+            operator = ".";
+        }
+        else {
+            switch (symbol) {
+                case "Addition" -> operator = "+";
+                case "Subtraction" -> operator = "-";
+                case "Multiplication" -> operator = "*";
+                case "Division" -> operator = "/";
+            }
+            num1 = Double.parseDouble(calculatorOutput.getText());
+            calculatorOutput.setText(String.valueOf(0.0));
+
+        }
+
 
     }
+
 }
